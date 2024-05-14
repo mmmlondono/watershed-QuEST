421daec951579df2f7328e2dfc4d2748d7d634c8
mmmlondono <manuelalg28@gmail.com>
2024-05-14 15:19:55 -0600

    Individual area plotting for leverage

--- scripts/leverage_plotting.R
+++ scripts/leverage_plotting.R
@@ -8,7 +8,14 @@ library(sf)
 library(sp)
 #for color palettes
 library(viridis)
-
+# for plotting with ggplot
+library(extrafont)
+library(ggplot2)
+library(ggspatial)
+library(patchwork)
+library(scico)
+#library(vapoRwave)
+library(tidyverse)
 
 ####load data####
 #read shapefiles
@@ -23,7 +30,10 @@ area1 <- st_read("data_geo/area1.shp")
 area3 <- st_read("data_geo/area3.shp")
 area5 <- st_read("data_geo/area5.shp")
 area6 <- st_read("data_geo/area6.shp")
-area12 <- st_read("data_geo/area12.shp")
+area13 <- st_read("data_geo/area13.shp")
+area14 <- st_read("data_geo/area14.shp")
+area15 <- st_read("data_geo/area15.shp")
+area16 <- st_read("data_geo/area16.shp")
 
 #load streams
 streams <- st_read("data_geo/area_stream_network.shp")
@@ -31,6 +41,7 @@ streams <- st_read("data_geo/area_stream_network.shp")
 #load leverage data  
 leverage <- read.csv("data/leverage.csv")
 #filter out by what you will use
+#DOC
 DOC <- leverage %>%
   dplyr::select(Sub_ProjectB, lat, lon, NPOC..mg.C.L.)
 #transform lat lon to geometries
@@ -39,17 +50,48 @@ DOC <- st_as_sf(DOC, coords = c("lon", "lat"), crs = '+proj=longlat +datum=WGS84
 levpoints <- DOC %>%
   dplyr::select(Sub_ProjectB, geometry)
 
+#TDN
+TDN <- leverage %>%
+  dplyr::select(Sub_ProjectB, lat, lon, TDN..mg.N.L.)
+#transform lat lon to geometries
+TDN <- st_as_sf(TDN, coords = c("lon", "lat"), crs = '+proj=longlat +datum=WGS84 +no_defs')
+
+levpoints <- TDN %>%
+  dplyr::select(Sub_ProjectB, geometry)
+
 
 ####plot on mapview####
 #make color palette
 pal <- magma(n = length(unique(lev$NPOC..mg.C.L.)), direction = -1)
 
-#plot
-mapview(area1, alpha.regions = 0., aplha = 1) + mapview(area, legend = FALSE, col.regions = pal) + 
+#plot DOC
+mapview(area1, alpha.regions = 0., alpha = 5, lwd = 3, color = "white", legend = FALSE) + 
+  mapview(area, col.regions = pal) + 
   mapview(streams, legend = FALSE) + 
-  mapview(DOC, cex = "NPOC..mg.C.L.", zcol = "NPOC..mg.C.L.")
+  mapview(DOC, cex = "NPOC..mg.C.L.", col.regions = "orange")
 
-####plotplot###
+#plot DOC with all areas
+mapview(area1, alpha.regions = 0., alpha = 5, lwd = 3, color = "white", legend = FALSE) + 
+  mapview(area3, col.regions = "lightgreen") + 
+  mapview(area5, col.regions = "tomato") +
+  mapview(area6, col.regions = "powderblue") +
+  mapview(area13, col.regions = "cornflowerblue") +
+  mapview(area14, col.regions = "yellow") +
+  mapview(area15, col.regions = "lightslateblue") +
+  mapview(area16, col.regions = "white") +
+  mapview(streams, legend = FALSE) + 
+  mapview(DOC, cex = "NPOC..mg.C.L.", col.regions = "hotpink")
+
+
+#plot TDN
+#map
+mapview(area1, alpha.regions = 0., alpha = 5, lwd = 3, color = "white", legend = FALSE) + 
+  mapview(area, col.regions = pal) + 
+  mapview(streams, legend = FALSE) + 
+  mapview(TDN, cex = "TDN..mg.N.L.", col.regions = "mediumaquamarine")
+
+
+####plot with tmap###
 #still very ugly and not finished
 sfws <- tm_shape(areas) +
   tm_borders(col = "black", lwd = 2) +
@@ -84,3 +126,27 @@ sfws <- tm_shape(areas) +
           title.fontface = "bold")
 sfws
 
+####plot with ggplot####
+###NPOC###
+combined_plot <- ggplot() +
+  geom_sf(data = area1, color = "black", fill = NA, cex = 5) +  # Outline area1
+  geom_sf(data = area,  aes(fill = shd_nwm), show.legend = FALSE) +         # Add area
+  geom_sf(data = DOC, aes(size = NPOC..mg.C.L.), alpha = 0.5) +  # Add DOC with fill
+  scale_fill_viridis_c(name = "NPOC (mg C/L)") +                 # Color scale for DOC
+  theme_minimal() +                                              # Minimal theme
+  ggtitle("Combined Map") +                                      # Title
+  theme(legend.position = "bottom")                              # Position the legend at the bottom
+#display
+print(combined_plot)
+
+###TDN###
+TDN <- ggplot() +
+  geom_sf(data = area1, color = "black", fill = NA, cex = 5) +  # Outline area1
+  geom_sf(data = area,  aes(fill = shd_nwm), show.legend = FALSE) +         # Add area
+  geom_sf(data = TDN, aes(size = TDN..mg.N.L.), alpha = 0.5) +  # Add DOC with fill
+  scale_fill_viridis_c(name = "TDN (mg C/L)") +                 # Color scale for DOC
+  theme_minimal() +                                              # Minimal theme
+  ggtitle("TDN") +                                      # Title
+  theme(legend.position = "bottom")                              # Position the legend at the bottom
+#display
+print(TDN)
